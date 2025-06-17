9f427573fda54534bb275720797aedbf499248b7
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-06-17 11:52:43 +0800

    Finish Step 3

--- java.js
+++ java.js
@@ -1,7 +1,13 @@
 function getComputerChoice(){
   let pick = ["rock", "paper", "scissors"];
-  let result = "";
-  return pick[Math.floor(Math.random() * pick.length)] + "";
+  
+  return pick[Math.floor(Math.random() * pick.length)];
 }
 
 console.log(getComputerChoice())
+
+function getHumanChoice(){
+    return prompt("pick yours");
+}
+
+console.log(getHumanChoice())
