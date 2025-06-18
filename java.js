e16708dc0b4bd90a013489182dcb34884c83585b
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-06-18 12:49:17 +0800

    Edit step 1

--- java.js
+++ java.js
@@ -1,3 +1,18 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+function getComputerChoice(){
+  
+ const pick = Math.floor(Math.random() * 3);
+ let computer;
+ 
+ if (pick === 0){
+  computer = "rock";
+ } else if (pick === 1){
+  computer = "paper";
+ } else {
+  computer = "scissors";
+ }
+
+  return computer;
+
 }
+
+console.log(getComputerChoice())
