fe45e8b830133a96249226a9384b81989c6df1b2
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-07-26 21:12:00 +0800

    try rps again

--- java.js
+++ java.js
@@ -1,54 +1,19 @@
 function getComputerChoice(){
-  
- const pick = Math.floor(Math.random() * 3);
- let computer;
- 
- if (pick === 0){
-  computer = "rock";
- } else if (pick === 1){
-  computer = "paper";
- } else {
-  computer = "scissors";
- }
-
-  return computer;
-
+  const pcChoice = Math.floor(Math.random() * 3);
+  if (pcChoice === 0){
+    return "rock";
+  } else if (pcChoice === 1){
+    return "paper";
+  } else {
+    return "scissors";
+  }
 }
-
-console.log(getComputerChoice())
 
 function getHumanChoice(){
-  const human = prompt("What's yours?");
-  return human;
+  const hoomanChoice = prompt("Which one do you pick?, Rock, Paper or Scissors?");
+  return hoomanChoice;
 }
-
-console.log(getHumanChoice())
 
 let humanScore = 0;
 let computerScore = 0;
 
-function playRound(humanChoice, computerChoice){
-
-
-
-    if (humanSelection == "rock" && computerSelection == "scissors"){
-      console.log('you win');
-    } else if (humanSelection == "paper" && computerSelection == "rock"){
-      console.log('you win')
-    } else if (humanSelection == "scissors" && computerSelection == "paper"){
-      console.log('win');
-    } else if (humanSelection == "rock" && computerSelection == "paper"){
-      console.log('you lose');
-    } else if (humanSelection == "paper" && computerSelection == "scissors"){
-      console.log('you lose')
-    } else if (humanSelection == "scissors" && computerSelection == "rock"){
-      console.log('lose');
-    } else {
-      console.log('tie, try again');
-    }
-}
-
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
