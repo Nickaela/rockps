2d02fa35157b7101e5c3dfe5ce06b61c71f7698a
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-07-26 22:44:23 +0800

    add step 5

--- java.js
+++ java.js
@@ -1,19 +1,34 @@
 function getComputerChoice(){
   const pcChoice = Math.floor(Math.random() * 3);
   if (pcChoice === 0){
-    return "rock";
+    console.log("rock");
   } else if (pcChoice === 1){
-    return "paper";
+    console.log("paper");
   } else {
-    return "scissors";
+    console.log("scissors");
   }
 }
 
 function getHumanChoice(){
   const hoomanChoice = prompt("Which one do you pick?, Rock, Paper or Scissors?");
-  return hoomanChoice;
+  console.log(hoomanChoice);
 }
 
 let humanScore = 0;
 let computerScore = 0;
 
+
+function playRound(humanChoice, computerChoice){
+  
+  if (humanChoice === 'rock' && computerChoice === 'scissors'){
+    console.log('You win, rock beats scissors');
+  } else {
+    console.log('try again');
+  }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
