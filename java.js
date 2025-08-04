792758e24778a698efdc7c94ade0c466d701ba14
Nickaela <68721084+Nickaela@users.noreply.github.com>
2025-08-04 20:55:39 +0800

    test the playGame function if can play 5 rounds and announce the winner at the end

--- java.js
+++ java.js
@@ -14,7 +14,6 @@ function getHumanChoice(){
  return hooman;
 }
 
-
 function playGame(){
 
 let humanScore = 0;
@@ -31,32 +30,36 @@ function playRound(humanChoice, computerChoice) {
   if (humanChoice === 'rock' && computerChoice === 'scissors'){
     console.log('you WIN, rock beats scissors');
     humanScore++;
-    console.log(game++);
+    console.log(++game);
     gameWinner();
   } else if (humanChoice === 'rock' && computerChoice === 'paper'){
     console.log('you LOSE, paper beats rock');
     computerScore++;
-    console.log(game++);
+    console.log(++game);
     gameWinner();
   } else if (humanChoice === 'paper' && computerChoice === 'rock'){
     console.log('you WIN, paper beats rock');
     humanScore++;
-    console.log(game++);
+    console.log(++game);;
+    gameWinner();
   } else if (humanChoice === 'paper' && computerChoice === 'scissors'){
     console.log('you LOSE, scissors beats paper');
     computerScore++;
-    console.log(game++);
+    console.log(++game);
+    gameWinner();
   } else if (humanChoice === 'scissors' && computerChoice === 'paper'){
     console.log('you WIN, scissors beats paper');
     humanScore++;
-    console.log(game++);
+    console.log(++game);
+    gameWinner();
   } else if (humanChoice === 'scissors' && computerChoice === 'rock'){
     console.log('you LOSE, rock beats scissors');
     computerScore++;
-    console.log(game++);
+    console.log(++game);
+    gameWinner();
   } else {
     console.log('its a tie');
-    console.log(game++);
+    console.log(++game);
     gameWinner();
   }
 }
@@ -74,9 +77,12 @@ function gameWinner(){
   if (game === 5){
     if (humanScore > computerScore){
       console.log('HUMANS wins');
-    } else {
+    } else if (computerScore > humanScore){
       console.log('COMPUTER wins');
+    } else {
+      console.log('NO WINNER');
     }
+    
     } else {
       playRound(a, b);
     }
